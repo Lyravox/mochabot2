2e06a5c01c24ac817897e1a23f031915807cf635
Juniper Watson <lyravox1@gmail.com>
2024-04-24 13:32:44 -0500

    add main.py

---
+++ bot/main.py
@@ -0,0 +1,29 @@
+import nextcord
+from nextcord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("TOKEN")
+
+intents =  nextcord.Intents.all()
+
+bot = commands.Bot(
+    intents=intents,
+    status=nextcord.Status.online,
+    activity=nextcord.Game(name="in development!")
+)
+
+bot.load_extension('cogs.information')
+bot.load_extension('cogs.fun')
+bot.load_extension('cogs.affirmation')
+bot.load_extension('cogs.moonphase')
+
+@bot.event
+async def on_ready():
+    print()
+    print(f"Logged in as {bot.user} [{bot.application_id}]")
+    print()
+    
+bot.run(token)
