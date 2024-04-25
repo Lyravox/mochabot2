f49a024d86cdaa09cb92e91c8a358b2990031b41
Juniper Watson <lyravox1@gmail.com>
2024-04-25 06:06:30 -0500

    Edit fun.py

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -4,8 +4,11 @@ from nextcord import Interaction, Member, SlashOption
 import os
 from dotenv import load_dotenv
 import aiohttp
+import random
 
 load_dotenv()
+
+apod = os.getenv("APOD_KEY")
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -56,5 +59,74 @@ class Fun(commands.Cog):
                     
                     await interaction.response.send_message("Could not fetch a duck image D:")
             
+    # Dice command
+    @nextcord.slash_command(description="Roll the dice.")
+    async def dice(
+        self,
+        interaction: Interaction,
+        sides: int = nextcord.SlashOption(
+            description="Number of sides on the die",
+            required=True,
+            min_value=2,
+            max_value=100
+        ),
+        amount: int = nextcord.SlashOption(
+            description="Number of diec to roll.",
+            required=True,
+            min_value=1,
+            max_value=100
+        )
+    ):
+        
+        rolls = [random.randint(1, sides) for _ in range(amount)]
+        string = ", ".join(str(roll) for roll in rolls)
+        
+        embed = nextcord.Embed(title="Dice", color=0x703c2f, description=f"You rolled some dice and got: {string}")
+        
+        await interaction.response.send_message(embed=embed)
+        
+    # Coinflip command
+    @nextcord.slash_command(description="Flips a coin")
+    async def coinflip(self, interaction: Interaction):
+        
+        result = random.choice(['heads', 'tails'])
+        
+        embed = nextcord.Embed(title=":coin: Coinflip :coin:", color=0x703c2f, description=f"You got {result}!")
+        
+        await interaction.response.send_message(embed=embed)
+        
+    # APOD command
+    @nextcord.slash_command(description="Provides the Astronomy Picture Of The Day (APOD)!")
+    async def apod(self, interaction: Interaction):
+        
+        url = "https://api.nasa.gov/planetary/apod"
+        
+        params = {'api_key': apod}
+        
+        async with aiohttp.ClientSession() as session:
+                        
+            async with session.get(url, params=params) as response:
+                
+                if response.status == 200:
+                    
+                    data = await response.json()
+                    
+                    image = data.get("url")
+                    explanation = data.get("explanation")
+
+                    embed = nextcord.Embed(title="Astronomy Picture Of The Day", color=0x703c2f, description=explanation)
+                    
+                    embed.set_image(url=image)
+                    
+                    await interaction.response.send_message(embed=embed)
+                    
+                else:
+                    
+                    error = await response.text()
+                    
+                    print(f"Failed to retrive APOD. Status: {response.status}, Error: {error}")
+                    
+                    await interaction.response.send_message("Failed to fetch APOD :(")
+        
 def setup(bot):
     bot.add_cog(Fun(bot))
